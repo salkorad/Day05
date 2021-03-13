a086d852d06678d3a7c9425803c1f84d2cd505d7
salkorad <salko.radovovic@gmail.com>
2021-03-13 01:57:27 +0100

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "My " + myNoun + " is not so " + myAdjective + " and can " + myVerb + " very " + myAdverb + ".";
+
+    //Only change code above this line
+  
+    return wordBlanks;
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
